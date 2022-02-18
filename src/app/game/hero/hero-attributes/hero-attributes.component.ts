87a2b05a7e05c840320e9ca3fa89df815858dd8d
Karol Padiasek <karol.padiasek@gmail.com>
2022-02-18 09:44:47 +0000

    hero service and display some data in hero attributes page

--- src/app/game/hero/hero-attributes/hero-attributes.component.ts
+++ src/app/game/hero/hero-attributes/hero-attributes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GameUIService } from '../../game-ui.service';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-attributes',
@@ -8,10 +9,17 @@ import { GameUIService } from '../../game-ui.service';
 })
 export class HeroAttributesComponent implements OnInit {
 
-  constructor(private gameUIService: GameUIService) { }
+  public heroData: any;
+
+  constructor(
+    private gameUIService: GameUIService,
+    private heroService: HeroService
+    ) { }
 
   ngOnInit() {
-
+    this.heroService.loadHeroData().subscribe(data => {
+      this.heroData = data.heroData;
+    });
   }
 
   closeModal(){
---
+++ src/app/game/hero/hero.service.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AppSettings } from 'src/app/AppSettings';
+
+@Injectable({providedIn: 'root'})
+export class HeroService {
+
+  constructor(private http: HttpClient){}
+
+  loadHeroData(){
+    return this.http.get<{'success': boolean; 'errorMessage': boolean; 'heroData': any}>(
+      AppSettings.API_ENDPOINT + '/hero/getFullData',
+      {responseType: 'json'}
+    );
+  }
+
+}
