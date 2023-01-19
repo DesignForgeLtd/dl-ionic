afff0ede82ea96fed4395254866c47368f40f0b1
Karol Padiasek <karol.padiasek@gmail.com>
2023-01-19 14:03:33 +0000

    live testing

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 export class AppSettings {
-  //public static  API_ENDPOINT = 'https://api.dragon-lords.com';
-   public static  API_ENDPOINT = 'http://dl-api.devel';
+  public static  API_ENDPOINT = 'https://api.dragon-lords.com';
+   //public static  API_ENDPOINT = 'http://dl-api.devel';
   //public static  API_ENDPOINT = 'http://dl-api.devel:8000';
 }
