c895e41afa9ae958c4354f82c02274b4d9730027
Sachem <karol.padiasek@gmail.com>
2023-10-24 07:12:29 +0100

    moved apiBaseURL to environment file

--- src/app/AppSettings.ts
+++ src/app/AppSettings.ts
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/naming-convention */
+import { environment } from './../environments/environment';
+
 export class AppSettings {
-  //  public static  API_ENDPOINT = 'https://api.dragon-lords.com';
-  public static  API_ENDPOINT = 'http://dl-api.devel';
-  //public static  API_ENDPOINT = 'http://dl-api.devel:8000';
+    public static  API_ENDPOINT = environment.apiBaseURL;
 }
