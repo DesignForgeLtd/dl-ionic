c41919d4c6cd28ecf6385f4081d1f6038a8ee340
Sachem <karol.padiasek@gmail.com>
2023-10-25 09:13:25 +0100

    sa

--- src/app/websockets/WS.ts
+++ src/app/websockets/WS.ts
@@ -1,5 +1,5 @@
 import { EventEmitter, Injectable, Output } from "@angular/core";
-import { environment } from './../environments/environment';
+import { environment } from './../../environments/environment';
 
 @Injectable({providedIn: 'root'})
 
